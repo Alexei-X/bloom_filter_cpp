398cfb6a881bebd2419efc4840f63e52221afbe7
alexbataille <alex.bataille@ulb.be>
2025-10-25 11:27:10 +0200

    First implementation of bloom filter finished

--- src/BloomFilter.hpp
+++ src/BloomFilter.hpp
@@ -7,17 +7,32 @@ class BloomFilter {
     protected:
         int m;
         int k;
-        std::vector<HashFunction<T>&> hash_fcts;
+        std::vector<std::unique_ptr<HashFunction<T>>> hash_fcts;
         HashFunctionFactory hash_fcts_fact;
+        std::vector<bool> bit_vector;
     public:
         BloomFilter(int m, int k) : m(m), k(k) {
             this->hash_fcts_fact = HashFunctionFactory();
             for (int i = 0; i < k; i++) {
                 hash_fcts.push_back(this->hash_fcts_fact.createHashFunction<T>(m));
             }
+            for (int j = 0; j < m; j++) bit_vector.push_back(false);
         }
         ~BloomFilter() = default;
-        bool insert(T element);
-        bool isInserted(T element);
+
+        bool insert(T element) {
+            if (isInserted(element)) return false;
+            for (int i = 0; i < this->k; i++) {
+                this->bit_vector.at(this->hash_fcts.at(i)->getIndex(element)) = true;
+            }
+            return true;
+        }
+        
+        bool isInserted(T element) {
+            for (int i = 0; i < this->k; i++) {
+                if (!this->bit_vector.at(this->hash_fcts.at(i)->getIndex(element))) return false;
+            }
+            return true;
+        }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
-#include "HashFunctionFactory.hpp"
+#include "BloomFilter.hpp"
 
 int main() {
 
-    std::cout << "Hello world !" << std::endl;
-    HashFunctionFactory factory = HashFunctionFactory();
-    std::unique_ptr<HashFunction<std::string>> fct1 = factory.createHashFunction<std::string>(500);
-    std::unique_ptr<HashFunction<std::string>> fct2 = factory.createHashFunction<std::string>(500);
-    std::unique_ptr<HashFunction<std::string>> fct3 = factory.createHashFunction<std::string>(500);
-    std::cout << fct1->getIndex("Bonjour") << std::endl;
-    std::cout << fct2->getIndex("Bonjour") << std::endl;
-    std::cout << fct3->getIndex("Bonjour") << std::endl;
-
-    std::vector<bool> vec = {true, false, false};
-    std::unique_ptr<HashFunction<std::vector<bool>>> fct_vec = factory.createHashFunction<std::vector<bool>>(100);
-    std::unique_ptr<HashFunction<std::vector<bool>>> fct_vec2 = factory.createHashFunction<std::vector<bool>>(100);
-    std::cout << fct_vec->getIndex(vec) << std::endl;
-    std::cout << fct_vec2->getIndex(vec) << std::endl;
+    BloomFilter<std::string> string_bloom = BloomFilter<std::string>(500, 10);
+    string_bloom.insert("Alex");
+    if (string_bloom.isInserted("Alex")) {
+        std::cout << "Alex Inserted" << std::endl;
+    }
+    if (string_bloom.isInserted("Bob")) {
+        std::cout << "Bob Inserted" << std::endl;
+    }
+    string_bloom.insert("Alexi");
+    if (string_bloom.isInserted("Alexi")) {
+        std::cout << "Alexi Inserted" << std::endl;
+    }
 
     return 0;
 }
